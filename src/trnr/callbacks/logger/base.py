4d32ba463b12b110a6ab8d3ac63908eac96073fe
nickeisenberg <nickeisenberg@gmail.com>
2024-07-28 11:00:24 -0700

    started on the logger

--- src/trnr/callbacks/logger/base.py
+++ src/trnr/callbacks/logger/base.py
@@ -3,16 +3,3 @@ from ..base import Callback
 
 class Logger(Callback, ABC):
     """Logger base class"""
-    def __init__(self):
-        super().__init__()
-
-        self._train_log = []
-        self._validation_log = []
-    
-    @property
-    def train_log(self):
-        return self._train_log
-
-    @property
-    def validation_log(self):
-        return self._validation_log
--- src/trnr/callbacks/logger/logger.py
+++ src/trnr/callbacks/logger/logger.py
@@ -1,10 +1,43 @@
+from collections import defaultdict
+import os
+import csv
+
 from ...trainer import Trainer
 from .base import Logger as _Logger
+from ..utils import rank_zero_only
 
-class Logger(_Logger):
-    def __init__(self):
+class CSVLogger(_Logger):
+    def __init__(self, log_dir: str = "logs"):
         super().__init__()
+    
+        self.log_dir = log_dir
+        self.epoch_log = defaultdict(list)
 
+    @rank_zero_only
+    def log(self, name: str, value: float):
+        self.epoch_log[name].append(value)
+    
+    @rank_zero_only
     def on_fit_start(self, trainer: Trainer):
+        self.log_dir = os.path.join(trainer.save_root, self.log_dir)
+        if not os.path.isdir(self.log_dir):
+            os.makedirs(self.log_dir)
+
+    @rank_zero_only
+    def before_train_epoch_pass(self, trainer: Trainer):
+        self._headers_written = False
         pass
 
+    @rank_zero_only
+    def after_train_epoch_pass(self, trainer: Trainer):
+        pass
+
+    @rank_zero_only
+    def after_validation_epoch_pass(self, trainer: Trainer):
+        pass
+
+    def _write_headers(self, which: str):
+        with open(f'{which}.csv', 'a', newline='',) as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=list(self.epoch_log.keys()))
+            writer.writeheader()
+        self._headers_written = True

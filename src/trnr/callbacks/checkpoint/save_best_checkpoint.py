254cf49e6a6ab508bab7bfcd32dabb9b50aa7953
nickeisenberg <nickeisenberg@gmail.com>
2024-08-02 11:57:02 -0700

    added a little to the save best checkpoint

--- src/trnr/callbacks/checkpoint/save_best_checkpoint.py
+++ src/trnr/callbacks/checkpoint/save_best_checkpoint.py
@@ -1,6 +1,32 @@
+from typing import Literal
+
+from ...trainer import Trainer
 from ..base import Callback
 
 class SaveBestCheckpoint(Callback):
     """A checkpoint saveer"""
-    def __init__(self):
+    def __init__(self,
+                 train_metric: str,
+                 train_desire: Literal["increase", "decrease"],
+                 validation_metric,
+                 validation_desire: Literal["increase", "decrease"]):
         super().__init__()
+
+        self.train_metric = train_metric
+        self.validation_metric = validation_metric
+        
+        if train_desire == "increase":
+            self.best_train_metric = -1e6
+        else:
+            self.best_train_metric = 1e6
+
+        if validation_desire == "increase":
+            self.best_validation_metric = -1e6
+        else:
+            self.best_validation_metric = 1e6
+
+    def after_train_epoch_pass(self, trainer: Trainer):
+        pass
+
+    def after_validation_epoch_pass(self, trainer: Trainer):
+        pass

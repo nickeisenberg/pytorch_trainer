61ece8145a3dfe84954cb25ec8847426489d1a42
nickeisenberg <nickeisenberg@gmail.com>
2024-08-11 10:17:16 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 A lightweight trainer that features callbacks and DDP support written with only
-840 lines of code.
+615 lines of core code.
+
+Easily
+  * log intra batch values to a csv as well as a tqdm progress bar,
+  * save the weights of a model based on an increase or decrease of a logged metric,
+  * train on multiple GPUs and multiple nodes with `DataDistributedParallel`,
+  * write any custom feature you would like with the use of a callback.
--- lines_of_code.py
+++ lines_of_code.py
@@ -1,18 +1,42 @@
 import os
 
-def print_lines_of_code():
+def print_core_lines_of_code():
     cwd = os.getcwd()
     lines = 0
     num_files = 0
     for root, _, files in os.walk(cwd):
         for f in files:
-            if f.endswith(".py") and not "examples" in root and "build" not in root and f != "lines_of_code.py":
+            if f.endswith(".py") \
+                and not "examples" in root \
+                and "build" not in root \
+                and f != "lines_of_code.py" \
+                and not "metrics" in root \
+                and not "lr_scheduler" in root \
+                and not "checkpoint" in root:
                 num_files += 1
                 path = os.path.join(root, f)
                 with open(path, "r") as file:
                     lines += len(file.readlines())
-    print("lines of code: ", lines)
-    print("number of files: ", num_files)
+    print("lines of core code: ", lines)
+    print("number of core files: ", num_files)
+
+def print_all_lines_of_code():
+    cwd = os.getcwd()
+    lines = 0
+    num_files = 0
+    for root, _, files in os.walk(cwd):
+        for f in files:
+            if f.endswith(".py") \
+                and not "examples" in root \
+                and "build" not in root \
+                and f != "lines_of_code.py":
+                num_files += 1
+                path = os.path.join(root, f)
+                with open(path, "r") as file:
+                    lines += len(file.readlines())
+    print("total lines of code: ", lines)
+    print("total number of files: ", num_files)
 
 if __name__ == "__main__":
-    print_lines_of_code()
+    print_core_lines_of_code()
+    print_all_lines_of_code()

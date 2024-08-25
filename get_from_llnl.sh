7668dbd217b7c7482788a327768e27b902bee5ca
nickeisenberg <nickeisenberg@gmail.com>
2024-08-25 13:39:41 -0700

    removed to files from being traceked

--- get_from_llnl.sh
+++
@@ -1,8 +0,0 @@
-rsync -av \
-    --exclude='.git' \
-    --exclude='*.pth' \
-    --exclude='*.png' \
-    --exclude='build' \
-    --exclude='*.egg-info' \
-    eisenbnt@pascal.llnl.gov:/p/lustre1/nova/software/pytorch_trainer \
-    /home/nicholas/GitRepos/
